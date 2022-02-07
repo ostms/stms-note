8c1c625a7b048500815aee87c84f989e56e2e172
Cocco.stms <boeo.stms@gmail.com>
2022-02-08 01:33:54 +0900

    220208 Update: モーダル実装、中身更新

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -1,18 +1,57 @@
 const section = document.getElementsByTagName('section');
 const modalWindow = document.getElementById('modal');
-
-for (let i = 0; i < section.length; i++) {
+// メモからモーダル起動
+for (let i=0; i < section.length; i++) {
 	section[i].addEventListener('click', () => {
 		let item = section[i].cloneNode(true);
 		modalWindow.appendChild(item);
 		modalWindow.classList.add('open');
 	});
 }
-modalWindow.addEventListener('click', ()=>{
-	if (modalWindow.classList.contains('open') == true) {
-		console.log(modalWindow.children);
-		let inSection = document.querySelector('#modal>section')
+// モーダル画面操作
+modalWindow.addEventListener('click', (event)=>{
+	// if (modalWindow.classList.contains('open') == true) {}
+	if(event.target.closest('#modal > section') === null){
+		// console.log(modalWindow.children);
 		modalWindow.classList.remove('open');
-		modalWindow.removeChild(inSection);
+		let inSection = document.querySelector('#modal > section');
+		let deleteIn = ()=>{
+			modalWindow.removeChild(inSection);
+		};
+		setTimeout(deleteIn, 400);
 	}
+});
+
+const Nav = document.querySelector('#navigation');
+const NavSwitch = document.getElementById('burger');
+// ナビメニュー操作 ---------------------------------
+NavSwitch.addEventListener('change', (Me)=>{
+	if (Me.target.checked === true) {
+		Nav.classList.add('open');
+	} else {
+		Nav.classList.remove('open');
+	}
+});
+
+const sectList = document.querySelectorAll('section > h2');
+const TOC = document.getElementById('toc');
+// ナビに目次追加
+(function createTOC(){
+	let title, li, a;
+	sectList.forEach(Me => {
+		title = Me.textContent;
+		a = document.createElement('a');
+		a.href = "#"+title;
+		a.textContent = title;
+		li = document.createElement('li');
+		li.appendChild(a);
+		TOC.appendChild(li);
+	});
+})();
+
+const TagList = document.querySelectorAll('header a');
+TagList.forEach( Me =>{
+	Me.addEventListener('click', (Event)=> {
+		console.log(Event.target);
+	});
 });
